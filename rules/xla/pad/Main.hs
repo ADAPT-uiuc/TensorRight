8a107aca42d3cf68984fd4bec7776b77d3266277
jaiarora0011 <jaiarora0011@gmail.com>
2025-06-10 02:34:13 -0500

    Rewrite Pad (X) => Slice(Pad(X)) with appropriate number of RClasses
    Earlier, the rule has 2 RClasses, meanwhile the rule needs 4 different
    behaviours of axes.

--- rules/xla/pad/Main.hs
+++ rules/xla/pad/Main.hs
@@ -82,44 +82,59 @@ rule03 _ = do
 
 rule04 :: forall a. AnyDTypeRule a
 rule04 _ = do
-  -- TODO: There should be 4 rclasses: combination of the signs of the
-  -- low and high paddings.
-  [rclass0, rclass1] <- newRClasses ["rclass0", "rclass1"]
+  -- rclass0: positive low, positive high
+  -- rclass1: positive low, negative high
+  -- rclass2: negative low, positive high
+  -- rclass3: negative low, negative high
+  [rclass0, rclass1, rclass2, rclass3] <- newRClasses ["rclass0", "rclass1", "rclass2", "rclass3"]
   [rc0Size, rc0Low, rc0High, rc0Int] <- newMaps ["rc0Size", "rc0Low", "rc0High", "rc0Int"] rclass0
   [rc1Size, rc1Low, rc1High, rc1Int] <- newMaps ["rc1Size", "rc1Low", "rc1High", "rc1Int"] rclass1
+  [rc2Size, rc2Low, rc2High, rc2Int] <- newMaps ["rc2Size", "rc2Low", "rc2High", "rc2Int"] rclass2
+  [rc3Size, rc3Low, rc3High, rc3Int] <- newMaps ["rc3Size", "rc3Low", "rc3High", "rc3Int"] rclass3
 
-  tA <- newTensor @a "A" [rclass0 --> rc0Size, rclass1 --> rc1Size]
+  tA <- newTensor @a "A"
+          [rclass0 --> rc0Size,
+           rclass1 --> rc1Size,
+           rclass2 --> rc2Size,
+           rclass3 --> rc3Size]
 
   lhs <-
     pad tA ("a" :: a) $
       Padding
-        { low = [rclass0 --> rc0Low, rclass1 --> rc1Low],
-          interior = [rclass0 --> rc0Int, rclass1 --> rc1Int],
-          high = [rclass0 --> rc0High, rclass1 --> rc1High]
+        { low = [rclass0 --> rc0Low, rclass1 --> rc1Low, rclass2 --> rc2Low, rclass3 --> rc3Low],
+          interior = [rclass0 --> rc0Int, rclass1 --> rc1Int, rclass2 --> rc2Int, rclass3 --> rc3Int],
+          high = [rclass0 --> rc0High, rclass1 --> rc1High, rclass2 --> rc2High, rclass3 --> rc3High]
         }
   precondition [rc0Int] $ \[i] -> i .== 0
   precondition [rc1Int] $ \[i] -> i .== 0
-  precondition [rc0Low] $ \[low0] -> low0 .< 0
-  precondition [rc0High] $ \[high0] -> high0 .< 0
-  precondition [rc1Low] $ \[low1] -> low1 .>= 0
-  precondition [rc1High] $ \[high1] -> high1 .>= 0
+  precondition [rc2Int] $ \[i] -> i .== 0
+  precondition [rc3Int] $ \[i] -> i .== 0
+  precondition [rc0Low] $ \[l] -> l .>= 0
+  precondition [rc0High] $ \[h] -> h .>= 0
+  precondition [rc1Low] $ \[l] -> l .>= 0
+  precondition [rc1High] $ \[h] -> h .< 0
+  precondition [rc2Low] $ \[l] -> l .< 0
+  precondition [rc2High] $ \[h] -> h .>= 0
+  precondition [rc3Low] $ \[l] -> l .< 0
+  precondition [rc3High] $ \[h] -> h .< 0
 
-  rc0Start <- combineMap "rc0Start" (\[x] -> abs x) [rc0Low]
-  rc0End <- combineMap "rc0End" sum [rc0Size, rc0High]
-  rc0Stride <- newConstMap "rc0Stride" 1 rclass0
+  rc1End <- combineMap "rc1End" (\[s, l, h] -> s + l + h) [rc1Size, rc1Low, rc1High]
+  rc2Start <- combineMap "rc2Start" (\[x] -> abs x) [rc2Low]
+  rc3Start <- combineMap "rc3Start" (\[x] -> abs x) [rc3Low]
+  rc3End <- combineMap "rc3End" sum [rc3Size, rc3High]
   rhs <-
     slice
       ( pad tA ("a" :: a) $
           Padding
-            { low = [rclass1 --> rc1Low],
-              interior = [rclass1 --> rc1Int],
-              high = [rclass1 --> rc1High]
+            { low = [rclass0 --> rc0Low, rclass1 --> rc1Low],
+              interior = [],
+              high = [rclass0 --> rc0High, rclass2 --> rc2High]
             }
       )
       $ Slice
-        { start = [rclass0 --> rc0Start],
-          end = [rclass0 --> rc0End],
-          strides = [rclass0 --> rc0Stride]
+        { start = [rclass2 --> rc2Start, rclass3 --> rc3Start],
+          end = [rclass1 --> rc1End, rclass3 --> rc3End],
+          strides = []
         }
 
   rewrite "Pad(A, val, negative_negative) ⇒ Slice(Pad(A, val, 0_0), abs(negative),negative+size)" lhs rhs
